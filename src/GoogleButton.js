6eb32f8dffaec6a6dfc202159d63f76ea14ceb8d
mannMae <daga4242@gmail.com>
2022-05-27 12:57:53 +0900

    feat: 정보 수신 성공

--- src/GoogleButton.js
+++ src/GoogleButton.js
@@ -1,9 +1,11 @@
-import React, { useCallback } from 'react';
-import GoogleLogin from 'react-google-login';
-import { validateTokenAndObtainSession } from './login';
+import React, { useCallback, useEffect } from "react";
+import GoogleLogin from "react-google-login";
+import { validateTokenAndObtainSession } from "./login";
+import env from "react-dotenv";
+import { gapi } from "gapi-script";
 
 const clientId =
-  '636788162607-l8mqgvl09o36q5h7umgaru0jtbskrd6r.apps.googleusercontent.com';
+  "636788162607-l8mqgvl09o36q5h7umgaru0jtbskrd6r.apps.googleusercontent.com";
 
 const GoogleButton = ({ onSocial }) => {
   //   const onSuccess = async (response) => {
@@ -23,23 +25,41 @@ const GoogleButton = ({ onSocial }) => {
   //     console.log(response);
   //   };
 
-  const onSuccess = useCallback((response) => {
-    const idToken = response.tokenId;
-    const data = {
-      email: response.profileObj.email,
-      first_name: response.profileObj.givenName,
-      last_name: response.profileObj.familyName,
-    };
+  // const onSuccess = useCallback((response) => {
+  //   const idToken = response.tokenId;
+  //   const data = {
+  //     email: response.profileObj.email,
+  //     first_name: response.profileObj.givenName,
+  //     last_name: response.profileObj.familyName,
+  //   };
 
-    validateTokenAndObtainSession({ data, idToken })
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
-  });
+  //   validateTokenAndObtainSession({ data, idToken })
+  //     .then((res) => console.log(res))
+  //     .catch((err) => console.log(err));
+  // });
 
-  const onFailure = (error) => {
-    console.log(error);
+  // const onFailure = (error) => {
+  //   console.log(error);
+  // };
+
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId,
+        scope: "email",
+      });
+    }
+
+    gapi.load("client:auth2", start);
+  }, []);
+
+  const onSuccess = (response) => {
+    console.log(response);
   };
 
+  const onFailure = (response) => {
+    console.log(response);
+  };
   return (
     <div>
       <GoogleLogin
