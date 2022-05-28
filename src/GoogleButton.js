1fb3d6319d7a4fb29ca71511fa6667ae6a0ac532
김재만 <daga4242@gmail.com>
2022-05-28 18:27:08 +0900

    feat: 클라이언트 ID 오류수정

--- src/GoogleButton.js
+++ src/GoogleButton.js
@@ -1,11 +1,47 @@
 import React, { useCallback, useEffect } from 'react';
 import GoogleLogin from 'react-google-login';
+import { validateTokenAndObtainSession } from './login';
+import env from 'react-dotenv';
 import { gapi } from 'gapi-script';
 
 const clientId =
   '463010698088-4165vl983gosnd12r6k701udsod84r0k.apps.googleusercontent.com';
 
 const GoogleButton = ({ onSocial }) => {
+  //   const onSuccess = async (response) => {
+  //     console.log(response);
+
+  //     const {
+  //       googleId,
+  //       profileObj: { email, name },
+  //     } = response;
+
+  //     await onSocial({
+  //       socialId: googleId,
+  //       socialType: 'google',
+  //       email,
+  //       nickname: name,
+  //     });
+  //     console.log(response);
+  //   };
+
+  // const onSuccess = useCallback((response) => {
+  //   const idToken = response.tokenId;
+  //   const data = {
+  //     email: response.profileObj.email,
+  //     first_name: response.profileObj.givenName,
+  //     last_name: response.profileObj.familyName,
+  //   };
+
+  //   validateTokenAndObtainSession({ data, idToken })
+  //     .then((res) => console.log(res))
+  //     .catch((err) => console.log(err));
+  // });
+
+  // const onFailure = (error) => {
+  //   console.log(error);
+  // };
+
   useEffect(() => {
     function start() {
       gapi.client.init({
@@ -13,7 +49,7 @@ const GoogleButton = ({ onSocial }) => {
         scope: 'email',
       });
     }
-    console.log(gapi.client.init);
+
     gapi.load('client:auth2', start);
   }, []);
 
@@ -28,6 +64,7 @@ const GoogleButton = ({ onSocial }) => {
     <div>
       <GoogleLogin
         clientId={clientId}
+        // reponseType={'id_token'}
         buttonText="구글아이디로 로그인하기"
         onSuccess={onSuccess}
         onFailure={onFailure}
