12b878f01334c7f13e46aecc54f0e678e6d1a77e
김재만 <daga4242@gmail.com>
2022-05-28 17:30:48 +0900

    refactor: GoogleButton Legacy코드 제거

--- src/GoogleButton.js
+++ src/GoogleButton.js
@@ -1,56 +1,20 @@
-import React, { useCallback, useEffect } from "react";
-import GoogleLogin from "react-google-login";
-import { validateTokenAndObtainSession } from "./login";
-import env from "react-dotenv";
-import { gapi } from "gapi-script";
+import React, { useCallback, useEffect } from 'react';
+import GoogleLogin from 'react-google-login';
+import { gapi } from 'gapi-script';
 
 const clientId =
-  "636788162607-l8mqgvl09o36q5h7umgaru0jtbskrd6r.apps.googleusercontent.com";
+  '636788162607-l8mqgvl09o36q5h7umgaru0jtbskrd6r.apps.googleusercontent.com';
 
 const GoogleButton = ({ onSocial }) => {
-  //   const onSuccess = async (response) => {
-  //     console.log(response);
-
-  //     const {
-  //       googleId,
-  //       profileObj: { email, name },
-  //     } = response;
-
-  //     await onSocial({
-  //       socialId: googleId,
-  //       socialType: 'google',
-  //       email,
-  //       nickname: name,
-  //     });
-  //     console.log(response);
-  //   };
-
-  // const onSuccess = useCallback((response) => {
-  //   const idToken = response.tokenId;
-  //   const data = {
-  //     email: response.profileObj.email,
-  //     first_name: response.profileObj.givenName,
-  //     last_name: response.profileObj.familyName,
-  //   };
-
-  //   validateTokenAndObtainSession({ data, idToken })
-  //     .then((res) => console.log(res))
-  //     .catch((err) => console.log(err));
-  // });
-
-  // const onFailure = (error) => {
-  //   console.log(error);
-  // };
-
   useEffect(() => {
     function start() {
       gapi.client.init({
         clientId,
-        scope: "email",
+        scope: 'email',
       });
     }
 
-    gapi.load("client:auth2", start);
+    gapi.load('client:auth2', start);
   }, []);
 
   const onSuccess = (response) => {
@@ -64,7 +28,6 @@ const GoogleButton = ({ onSocial }) => {
     <div>
       <GoogleLogin
         clientId={clientId}
-        // reponseType={'id_token'}
         buttonText="구글아이디로 로그인하기"
         onSuccess={onSuccess}
         onFailure={onFailure}
