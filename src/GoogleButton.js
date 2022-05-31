555b9f3e3104155d1e0278ec98671208b32cd2e4
김재만 <daga4242@gmail.com>
2022-05-31 13:30:02 +0900

    feat: 로그아웃 구현

--- src/GoogleButton.js
+++ src/GoogleButton.js
@@ -1,10 +1,11 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import GoogleLogin from 'react-google-login';
 import { gapi } from 'gapi-script';
 import { useDispatch } from 'react-redux';
 import JWTAuth from './services/auth';
 import http from './api/http';
 import { API_ENDPOINTS } from './api/ApiEndpoint';
+import styled from 'styled-components';
 
 const clientId =
   '463010698088-4165vl983gosnd12r6k701udsod84r0k.apps.googleusercontent.com';
@@ -44,6 +45,7 @@ const GoogleButton = ({ onSocial }) => {
   // const onFailure = (error) => {
   //   console.log(error);
   // };
+  const [isLogin, setIsLogin] = useState(false);
 
   useEffect(() => {
     function start() {
@@ -58,21 +60,23 @@ const GoogleButton = ({ onSocial }) => {
 
   const onSuccess = (response) => {
     http
-      .post(API_ENDPOINTS.LOGIN, {
-        access_token: response.tokenId,
-      })
+      .post(
+        API_ENDPOINTS.LOGIN,
+        {
+          google_token: response.tokenId,
+        },
+        { withCredentials: true }
+      )
       .then((res) => {
-        const userInfo = {};
-        let accessToken = res.headers.accessToken;
+        let accessToken = res.data.token.access_token;
         console.log(res);
         localStorage.setItem('accessToken', accessToken);
-
+        setIsLogin(true);
         //   history.push('/');
       })
       .catch((error) => {
         console.log(error);
       });
-
     // dispatch(JWTAuth.onLogin(response.tokenId));
     console.log(response);
   };
@@ -80,17 +84,50 @@ const GoogleButton = ({ onSocial }) => {
   const onFailure = (response) => {
     console.log(response);
   };
+
+  const onLogoutSuccess = () => {
+    setIsLogin(false);
+    console.log(isLogin);
+
+    const access_token = localStorage.getItem('accessToken');
+    // function deleteCookie(name) {
+    //   document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    // }
+    http
+      .post(API_ENDPOINTS.LOGOUT, { access_token })
+      .then((res) => {
+        localStorage.removeItem('accessToken');
+        // deleteCookie('access_token');
+        window.alert('로그아웃 되었습니다.');
+        setIsLogin(false);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  };
   return (
     <div>
-      <GoogleLogin
-        clientId={clientId}
-        // reponseType={'id_token'}
-        buttonText="구글아이디로 로그인하기"
-        onSuccess={onSuccess}
-        onFailure={onFailure}
-      />
+      {!isLogin ? (
+        <GoogleLogin
+          clientId={clientId}
+          // reponseType={'id_token'}
+          buttonText="구글아이디로 로그인하기"
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+        />
+      ) : (
+        <LogoutButton onClick={onLogoutSuccess}>로그아웃</LogoutButton>
+      )}
     </div>
   );
 };
 
 export default GoogleButton;
+
+const LogoutButton = styled.button`
+  background-color: #fff;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  width: 200px;
+  height: 30px;
+  cursor: pointer;
+`;
--- src/api/ApiEndpoint.js
+++ src/api/ApiEndpoint.js
@@ -1,3 +1,4 @@
 export const API_ENDPOINTS = {
   LOGIN: '/users/google/login',
+  LOGOUT: '/users/logout',
 };
